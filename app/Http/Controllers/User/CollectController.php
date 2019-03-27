5034afa205b37dc530e17a8d9470df8e17565393
166660614 <166660614@qq.com>
2019-03-27 16:13:12 +0800

    das

--- app/Http/Controllers/User/CollectController.php
+++ app/Http/Controllers/User/CollectController.php
@@ -11,17 +11,17 @@ class CollectController extends Controller
     public function collectAdd(){
         $timestamps=$_POST['timestamps'];
         $user_id=$_POST['user_id'];
-        $coll_key='collecion:redis:';
-        $res=Redis::zIncrBy($coll_key,$timestamps,$user_id);
+        $coll_key='collecion:user:'.$user_id;
+        $res=Redis::zAdd($coll_key,$timestamps,$user_id);
         if($res){
             $data=[
                 'errcode'=>4001,
-                'errmsg'=>'ok'
+                'errmsg'=>'收藏成功'
             ];
         }else{
             $data=[
                 'errcode'=>5001,
-                'errmsg'=>'no'
+                'errmsg'=>'您已经收藏过了'
             ];
         }
         return $data;
