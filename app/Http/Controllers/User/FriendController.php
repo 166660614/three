27a7d9d323d0f6cb72c6ad905ad90123d4e4862e
166660614 <166660614@qq.com>
2019-03-29 11:28:46 +0800

    dada

--- app/Http/Controllers/User/FriendController.php
+++ app/Http/Controllers/User/FriendController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Model\FriendModel;
+use App\Model\UserFriendModel;
 class FriendController extends Controller
 {
     public function friendAdd(){
